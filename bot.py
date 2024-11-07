e51061b70c05fc3d6498f5d1f93f7a925071cbd2
akiidjk <dragonfaiver@gmail.com>
2024-11-07 22:22:26 +0100

    Implement Discord bot with command handling and status updates

---
+++ bot.py
@@ -0,0 +1,232 @@
+import json
+import os
+import platform
+import random
+import sys
+
+import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+from discord import app_commands
+from dotenv import load_dotenv
+
+from lib.logger import logger
+
+if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+        config = json.load(file)
+
+"""	
+Setup bot intents (events restrictions)
+For more information about intents, please go to the following websites:
+https://discordpy.readthedocs.io/en/latest/intents.html
+https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
+
+
+Default Intents:
+intents.bans = True
+intents.dm_messages = True
+intents.dm_reactions = True
+intents.dm_typing = True
+intents.emojis = True
+intents.emojis_and_stickers = True
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.guild_scheduled_events = True
+intents.guild_typing = True
+intents.guilds = True
+intents.integrations = True
+intents.invites = True
+intents.messages = True # `message_content` is required to get the content of the messages
+intents.reactions = True
+intents.typing = True
+intents.voice_states = True
+intents.webhooks = True
+
+Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
+intents.members = True
+intents.message_content = True
+intents.presences = True
+"""
+
+intents = discord.Intents.all()
+
+"""
+Uncomment this if you want to use prefix (normal) commands.
+It is recommended to use slash commands and therefore not use prefix commands.
+
+If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
+"""
+# intents.message_content = True
+
+# Setup both of the loggers
+
+
+class DiscordBot(commands.Bot):
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix=commands.when_mentioned_or(config["prefix"]),
+            intents=intents,
+            help_command=None,
+        )
+        """
+        This creates custom bot variables so that we can access these variables in cogs more easily.
+
+        For example, The config is available using the following code:
+        - self.config # In this class
+        - bot.config # In this file
+        - self.bot.config # In cogs
+        """
+        self.logger = logger
+        self.config = config
+        self.database = None
+
+    async def load_cogs(self) -> None:
+        """
+        The code in this function is executed whenever the bot will start.
+        """
+        for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
+            if file.endswith(".py"):
+                extension = file[:-3]
+                try:
+                    await self.load_extension(f"cogs.{extension}")
+                    self.logger.info(f"Loaded extension '{extension}'")
+                except Exception as e:
+                    exception = f"{type(e).__name__}: {e}"
+                    self.logger.error(
+                        f"Failed to load extension {extension}\n{exception}"
+                    )
+
+    @tasks.loop(minutes=1.0)
+    async def status_task(self) -> None:
+        """
+        Setup the game status task of the bot.
+        """
+        statuses = ["🍪", "cooking the cookies", "flagging your system!"]
+        await self.change_presence(activity=discord.Game(random.choice(statuses)))
+
+    @status_task.before_loop
+    async def before_status_task(self) -> None:
+        """
+        Before starting the status changing task, we make sure the bot is ready
+        """
+        await self.wait_until_ready()
+
+    async def setup_hook(self) -> None:
+        """
+        This will just be executed when the bot starts the first time.
+        """
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {discord.__version__}")
+        self.logger.info(f"Python version: {platform.python_version()}")
+        self.logger.info(
+            f"Running on: {platform.system()} {platform.release()} ({os.name})"
+        )
+        self.logger.info("-------------------")
+        await self.load_cogs()
+        synced = await self.tree.sync()
+        self.logger.info(
+            f"Synced {len(synced)} commands: {', '.join([sync.name for sync in synced])}"
+        )
+        self.status_task.start()
+
+    async def on_message(self, message: discord.Message) -> None:
+        """
+        The code in this event is executed every time someone sends a message, with or without the prefix
+
+        :param message: The message that was sent.
+        """
+        if message.author == self.user or message.author.bot:
+            return
+        await self.process_commands(message)
+
+    async def on_command_completion(self, context: Context) -> None:
+        """
+        The code in this event is executed every time a normal command has been *successfully* executed.
+
+        :param context: The context of the command that has been executed.
+        """
+        full_command_name = context.command.qualified_name
+        split = full_command_name.split(" ")
+        executed_command = str(split[0])
+        if context.guild is not None:
+            self.logger.info(
+                f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
+            )
+        else:
+            self.logger.info(
+                f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs"
+            )
+
+    async def on_command_error(self, context: Context, error) -> None:
+        """
+        The code in this event is executed every time a normal valid command catches an error.
+
+        :param context: The context of the normal command that failed executing.
+        :param error: The error that has been faced.
+        """
+        if isinstance(error, commands.CommandOnCooldown):
+            minutes, seconds = divmod(error.retry_after, 60)
+            hours, minutes = divmod(minutes, 60)
+            hours = hours % 24
+            embed = discord.Embed(
+                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.NotOwner):
+            embed = discord.Embed(
+                description="You are not the owner of the bot!", color=0xE02B2B
+            )
+            await context.send(embed=embed)
+            if context.guild:
+                self.logger.warning(
+                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the guild {context.guild.name} (ID: {context.guild.id}), but the user is not an owner of the bot."
+                )
+            else:
+                self.logger.warning(
+                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the bot's DMs, but the user is not an owner of the bot."
+                )
+        elif isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                description="You are missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to execute this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(
+                description="I am missing the permission(s) `"
+                + ", ".join(error.missing_permissions)
+                + "` to fully perform this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title="Error!",
+                # We need to capitalize because the command arguments have no capital letter in the code and they are the first word in the error message.
+                description=str(error).capitalize(),
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        else:
+            raise error
+
+    async def on_scheduled_event_update(self, before, after):
+        if (
+            before.status != after.status
+            and after.status == discord.EventStatus.completed
+        ):
+            channel = after.guild.system_channel
+            if channel:
+                await channel.send(f"L'evento '{after.name}' è appena terminato!")
+
+
+load_dotenv()
+
+bot = DiscordBot()
+bot.run(os.getenv("TOKEN"))
