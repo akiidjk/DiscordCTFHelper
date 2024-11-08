918094daf60185e9dd7e26168510ce4100ac084d
akiidjk <dragonfaiver@gmail.com>
2024-11-08 11:14:05 +0100

    Add CTFModel class to represent CTF data structure

---
+++ lib/ctf_model.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class CTFModel:
+    """CTF model class"""
+
+    name: str
+    description: str
+    text_channel_id: int
+    event_id: int
+    role_id: int
