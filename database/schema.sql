e204898efa92f565c8abc46248bc99eed6ff0249
akiidjk <dragonfaiver@gmail.com>
2024-11-11 20:47:56 +0100

    Add server table and update CTF schema to include server_id foreign key

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,17 @@
-CREATE TABLE IF NOT EXISTS 'ctf' (
-  'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-  'name' TEXT NOT NULL,
-  'description' TEXT NOT NULL,
-  'text_channel_id' INTEGER NOT NULL,
-  'event_id' INTEGER NOT NULL,
-  'role_id' INTEGER NOT NULL,
-  'msg_id' INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS server (
+  id INTEGER PRIMARY KEY,
+  active_category_id INTEGER NOT NULL,
+  archive_category_id INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ctf (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  server_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  text_channel_id INTEGER NOT NULL,
+  event_id INTEGER NOT NULL,
+  role_id INTEGER NOT NULL,
+  msg_id INTEGER NOT NULL,
+  FOREIGN KEY (server_id) REFERENCES server (id)
 );
