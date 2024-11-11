fd32017a2c96bedc3c3714acb1057249567b45df
akiidjk <dragonfaiver@gmail.com>
2024-11-11 20:48:01 +0100

    Rename ctf_model.py to models.py and add ServerModel class for improved structure

--- lib/models.py
+++ lib/models.py
@@ -5,9 +5,19 @@ from dataclasses import dataclass
 class CTFModel:
     """CTF model class"""
 
+    server_id: int
     name: str
     description: str
     text_channel_id: int
     event_id: int
     role_id: int
     msg_id: int
+
+
+@dataclass(frozen=True)
+class ServerModel:
+    """Server model class"""
+
+    id: int
+    active_category_id: int
+    archive_category_id: int
