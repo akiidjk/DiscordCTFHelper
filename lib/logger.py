d07a598fd51ac8aeb18a6a4f927294bec59ed9f2
akiidjk <dragonfaiver@gmail.com>
2024-11-12 17:51:31 +0100

    Refactor LoggingFormatter to improve type hinting and enhance log formatting logic

--- lib/logger.py
+++ lib/logger.py
@@ -1,4 +1,5 @@
 import logging
+from typing import ClassVar
 
 
 class LoggingFormatter(logging.Formatter):
@@ -12,8 +13,7 @@ class LoggingFormatter(logging.Formatter):
     # Styles
     reset = "\x1b[0m"
     bold = "\x1b[1m"
-
-    COLORS = {
+    COLORS: ClassVar[dict[int, str]] = {
         logging.DEBUG: gray + bold,
         logging.INFO: blue + bold,
         logging.WARNING: yellow + bold,
@@ -23,12 +23,12 @@ class LoggingFormatter(logging.Formatter):
 
     def format(self, record):
         log_color = self.COLORS[record.levelno]
-        format = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
-        format = format.replace("(black)", self.black + self.bold)
-        format = format.replace("(reset)", self.reset)
-        format = format.replace("(levelcolor)", log_color)
-        format = format.replace("(green)", self.green + self.bold)
-        formatter = logging.Formatter(format, "%Y-%m-%d %H:%M:%S", style="{")
+        format_log_record = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        format_log_record = format_log_record.replace("(black)", self.black + self.bold)
+        format_log_record = format_log_record.replace("(reset)", self.reset)
+        format_log_record = format_log_record.replace("(levelcolor)", log_color)
+        format_log_record = format_log_record.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(format_log_record, "%Y-%m-%d %H:%M:%S", style="{")
         return formatter.format(record)
 
 
@@ -40,9 +40,7 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(LoggingFormatter())
 # File handler
 file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-file_handler_formatter = logging.Formatter(
-    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
-)
+file_handler_formatter = logging.Formatter("[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{")
 file_handler.setFormatter(file_handler_formatter)
 
 # Add the handlers
